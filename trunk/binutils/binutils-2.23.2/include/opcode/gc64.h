39c4ba2ae9893cc9c59d6aedfdf11655cb81569f
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-07-03 22:21:06 +0000

    adding gc64 headers

---
+++ trunk/binutils/binutils-2.23.2/include/opcode/gc64.h
@@ -0,0 +1,24 @@
+/* cr16.h -- Header file for CR16 opcode and register tables.
+   Copyright 2007, 2008, 2010 Free Software Foundation, Inc.
+   Contributed by M R Swami Reddy
+
+   This file is part of GAS, GDB and the GNU binutils.
+
+   GAS, GDB, and GNU binutils is free software; you can redistribute it
+   and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3, or (at your
+   option) any later version.
+
+   GAS, GDB, and GNU binutils are distributed in the hope that they will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+#ifndef _GC64_H_
+#define _GC64_H_
+
+#endif /* _GC64_H_ */
