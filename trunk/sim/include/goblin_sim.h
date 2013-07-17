f59adb6f1aeb0a32f49fddeb6abf5439b8c02fd9
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-07-17 19:44:10 +0000

    adding native, simple disassembler

--- trunk/sim/include/goblin_sim.h
+++ trunk/sim/include/goblin_sim.h
@@ -27,6 +27,34 @@ extern void 	*gsim_malloc( size_t sz );
 
 extern int	gsim_config_read( struct gsim_t *sim );
 
+extern int	gsim_disass(	uint64_t *inst, 
+				uint32_t *l_r0, 
+				uint32_t *l_r1, 
+				uint32_t *l_r2, 
+				uint32_t *l_opc, 
+				uint32_t *l_ctr, 
+				uint32_t *l_ctr_vec, 
+				uint32_t *l_ctr_v0, 
+				uint32_t *l_ctr_v1, 
+				uint32_t *l_ctr_brk, 
+				uint32_t *l_ctr_imm8, 
+				uint32_t *l_ctr_imm4, 
+				int32_t  *l_imm4, 
+				uint64_t *l_imm8, 
+				uint32_t *u_r0,
+				uint32_t *u_r1, 
+				uint32_t *u_r2, 
+				uint32_t *u_opc, 
+				uint32_t *u_ctr, 
+				uint32_t *u_ctr_vec, 
+				uint32_t *u_ctr_v0, 
+				uint32_t *u_ctr_v1, 
+				uint32_t *u_ctr_brk, 
+				uint32_t *u_ctr_imm8, 
+				uint32_t *u_ctr_imm4, 
+				int32_t  *u_imm4, 
+				uint64_t *u_imm8 );
+
 #ifdef __cplusplus
 } /* extern C */
 #endif
