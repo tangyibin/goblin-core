66067bce6c7851585e7e54e123e42b87ec8d6af7
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-07-18 16:20:29 +0000

    adding the i/o functions to write binary object file

--- trunk/sim/src/gasm_io.c
+++ trunk/sim/src/gasm_io.c
@@ -22,12 +22,34 @@
 extern int gasm_write_object( char *ofile, uint64_t *inter, uint64_t nread )
 {
 	/* vars */
+	uint64_t i	= 0x00ll;
+	FILE *outfile	= NULL;
 	/* ---- */
 
 #ifdef GSIM_DEBUG
 	GSIM_PRINT_FUNC_ENTRY();
 #endif
 
+	/* 
+	 * open the file 
+	 */
+	outfile = fopen( ofile, "w+b" );
+	if( ofile != NULL ){ 
+		GSIM_PRINT_ERROR( "GASM_ERROR : Could not open object file" );
+		return -1;
+	}
+
+	/* 
+	 * write all the instructions
+	 */
+	for( i=0; i<nread; i++ ){ 
+		fwrite( &(inter[i]), sizeof( uint64_t ), 1, outfile );
+	}	
+
+	/* 
+	 * close the file 
+	 */
+	fclose( outfile );
 
 #ifdef GSIM_DEBUG
 	GSIM_PRINT_FUNC_EXIT();
