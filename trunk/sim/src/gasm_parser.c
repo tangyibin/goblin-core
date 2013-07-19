629972c0bb05c50638d42006659fd3f1a1fd39fa
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-07-19 01:11:54 +0000

    working through the first portion of the parser

--- trunk/sim/src/gasm_parser.c
+++ trunk/sim/src/gasm_parser.c
@@ -23,7 +23,10 @@ extern uint64_t gasm_parser( char *afile, uint64_t *inter, uint64_t nrows )
 {
 	/* vars */
 	FILE *ifile	= NULL;
+	char *buf	= NULL;
 	uint64_t insts	= 0x00ll;
+	uint64_t tmp	= 0x00ll;
+	int imm		= 0;
 	/* ---- */
 
 #ifdef GSIM_DEBUG
@@ -39,8 +42,32 @@ extern uint64_t gasm_parser( char *afile, uint64_t *inter, uint64_t nrows )
 		return 0x00ll;
 	}
 
+	/* 
+	 * rewind the file 
+	 */
 	rewind( ifile );
 
+	buf = gsim_malloc( sizeof( char ) * 2048 );
+	memset( buf, 0, sizeof( char ) * 2048 );
+
+	/* 
+	 * Parse the file
+	 */
+	while( !feof( ifile ) ){
+	
+		fgets( buf, 2048, ifile );
+
+		if( (strlen( buf ) > 2 ) && (buf[0] != '#') ){ 
+
+			/* 
+			 * this buffer is an instruction 
+			 */		
+		}
+
+		memset( buf, 0, sizeof( char ) * 2048 );
+		imm = 0;
+		tmp = 0x00ll;
+	}
 
 	/* 
 	 * close the file
