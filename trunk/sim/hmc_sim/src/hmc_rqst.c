0a1b482aced801faab547b491888d43b2486cede
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-07-31 03:18:38 +0000

    adding memory request handler skeleton

---
+++ trunk/sim/hmc_sim/src/hmc_rqst.c
@@ -0,0 +1,27 @@
+/* 
+ * _HMC_RQST_C_
+ * 
+ * HYBRID MEMORY CUBE SIMULATION LIBRARY 
+ * 
+ * MEMORY REQUEST HANDLERS
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hmc_sim.h"
+
+
+/* ----------------------------------------------------- HMCSIM_MEMREQUEST */
+/* 
+ * HMCSIM_MEMREQUEST
+ * 
+ */
+extern int	hmcsim_memrequest( struct hmcsim_t *hmc )
+{
+	return 0;
+}
+
+/* EOF */
