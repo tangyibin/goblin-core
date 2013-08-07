34aad81165965faa4394037fc2f0d599d7044789
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-08-07 03:27:55 +0000

    adding clock function for clock instantiation

---
+++ sim/hmc_sim/src/hmc_clock.c
@@ -0,0 +1,31 @@
+/* 
+ * _HMC_CLOCK_C_
+ * 
+ * HYBRID MEMORY CUBE SIMULATION LIBRARY 
+ * 
+ * CLOCK FUNCTIONS
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hmc_sim.h"
+
+
+/* ----------------------------------------------------- HMCSIM_CLOCK */
+/* 
+ * HMCSIM_CLOCK
+ * 
+ */
+extern int	hmcsim_clock( struct hmcsim_t *hmc )
+{
+	if( hmc == NULL ){ 
+		return -1;
+	}
+
+	return 0;
+}
+
+/* EOF */
