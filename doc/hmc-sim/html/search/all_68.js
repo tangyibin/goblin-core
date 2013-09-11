557ff1320693b0ad7899d7ff644d9b4cebd92f9e
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-09-11 21:39:52 +0000

    updating the docs based upon the latest code changes

--- doc/hmc-sim/html/search/all_68.js
+++ doc/hmc-sim/html/search/all_68.js
@@ -15,9 +15,12 @@ var searchData=
   ['hmcsim_5fdecode_5fmemresponse',['hmcsim_decode_memresponse',['../hmc__sim_8h.html#aa1fcfa11c6a84e6881bcb3300264f27f',1,'hmc_sim.h']]],
   ['hmcsim_5ffree',['hmcsim_free',['../hmc__sim_8h.html#a92976e293a2ab29a590d00eb4145a826',1,'hmc_sim.h']]],
   ['hmcsim_5finit',['hmcsim_init',['../hmc__sim_8h.html#aa800472d739e996abc58805909ff66c1',1,'hmc_sim.h']]],
+  ['hmcsim_5fjtag_5freg_5fread',['hmcsim_jtag_reg_read',['../hmc__sim_8h.html#a17c848a15bdf0009329b5486df1c0f66',1,'hmc_sim.h']]],
+  ['hmcsim_5fjtag_5freg_5fwrite',['hmcsim_jtag_reg_write',['../hmc__sim_8h.html#a9a3f8d88943e73f19bef1978f32b190c',1,'hmc_sim.h']]],
   ['hmcsim_5frecv',['hmcsim_recv',['../hmc__sim_8h.html#ab2739ca92af423efe975b966492dfd7c',1,'hmc_sim.h']]],
   ['hmcsim_5fsend',['hmcsim_send',['../hmc__sim_8h.html#a12313ad659378fbf0f53e19ba6ee9eed',1,'hmc_sim.h']]],
   ['hmcsim_5ft',['hmcsim_t',['../structhmcsim__t.html',1,'']]],
   ['hmcsim_5ftrace_5fhandle',['hmcsim_trace_handle',['../hmc__sim_8h.html#aa6538f5a29e5aa16903e55f7e6c3c3b9',1,'hmc_sim.h']]],
+  ['hmcsim_5ftrace_5fheader',['hmcsim_trace_header',['../hmc__sim_8h.html#a9b0cc2e32a3445aa840ab91acd807d22',1,'hmc_sim.h']]],
   ['hmcsim_5ftrace_5flevel',['hmcsim_trace_level',['../hmc__sim_8h.html#ab08eceb0130f5b1f0fa9bd8eb2419767',1,'hmc_sim.h']]]
 ];
