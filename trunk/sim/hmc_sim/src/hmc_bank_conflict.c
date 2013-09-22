e460fe557a6c50ff25ef6a132554adff8ce8b3c1
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-09-22 18:49:51 +0000

    small formatting updates

--- trunk/sim/hmc_sim/src/hmc_bank_conflict.c
+++ trunk/sim/hmc_sim/src/hmc_bank_conflict.c
@@ -15,6 +15,13 @@
 
 /* ----------------------------------------------------- FUNCTION PROTOTYPES */
 extern int	hmcsim_trace( struct hmcsim_t *hmc, char *str );
+extern int	hmcsim_trace_bank_conflict( 	struct hmcsim_t *hmc, 
+						uint32_t dev, 
+						uint32_t quad, 
+						uint32_t vault, 
+						uint32_t bank, 
+						uint64_t addr1, 
+						uint64_t addr2 );
 
 
 
@@ -30,6 +37,13 @@ extern int	hmcsim_process_bank_conflicts( struct hmcsim_t *hmc,
 						uint32_t vault, 
 						uint64_t *addr )
 {
+	/* vars */
+	/* ---- */
+
+	/* 
+	 * sanity check 
+	 *
+	 */
 	if( hmc == NULL ){ 
 		return -1;
 	}
