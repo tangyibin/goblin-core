84bc5098b81845b8759e865dbefe8c4a9e5ce64f
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2013-09-27 02:04:47 +0000

    repaired clock functions to decode vault+quad correctly; repaired quad+vault decode functions

--- trunk/sim/hmc_sim/src/hmc_util.c
+++ trunk/sim/hmc_sim/src/hmc_util.c
@@ -66,6 +66,144 @@ extern int hmcsim_util_decode_slid(	struct hmcsim_t *hmc,
 	return 0;
 }
 
+/* ----------------------------------------------------- HMCSIM_UTIL_DECODE_QUAD */
+/* 
+ * HMCSIM_UTIL_DECODE_QUAD
+ * 
+ */
+extern int hmcsim_util_decode_quad( 	struct hmcsim_t *hmc, 
+					uint32_t dev, 
+					uint32_t bsize, 
+					uint64_t addr,
+					uint32_t *quad )
+{
+	/* vars */
+	uint32_t num_links	= 0x00;
+	uint32_t capacity	= 0x00;
+	uint32_t tmp		= 0x00;
+	/* ---- */
+
+	/*
+	 * sanity check 
+	 * 
+	 */
+	if( hmc == NULL ){ 
+		return -1;
+	}
+
+	num_links	= hmc->num_links;
+	capacity	= hmc->capacity;	
+
+	/* 
+	 * link layout 
+	 * 
+	 */
+	if( num_links == 4 ){ 
+		/* 
+	 	 * 4-link device 
+		 *
+		 */
+		if( capacity == 2 ){
+
+			switch( bsize )
+			{
+				case 32:
+					/* [8:7] */
+					tmp = (uint32_t)((addr>>7) & 0x3);
+					break;
+				case 64:
+					/* [9:8] */
+					tmp = (uint32_t)((addr>>8) & 0x3);
+					break;
+				case 128:
+					/* [10:9] */
+					tmp = (uint32_t)((addr>>9) & 0x3);
+					break;
+				default:
+					break;
+			}
+
+		} else if( capacity == 4 ){ 
+
+			switch( bsize )
+			{
+				case 32:
+					/* [12:11] */
+					tmp = (uint32_t)((addr>>11) & 0x3);
+					break;
+				case 64:
+					/* [13:12] */
+					tmp = (uint32_t)((addr>>12) & 0x3);
+					break;
+				case 128:
+					/* [14:13] */
+					tmp = (uint32_t)((addr>>13) & 0x3);
+					break;
+				default:
+					break;
+			}
+
+		}
+	} else if( num_links == 8 ){ 
+		/* 
+	 	 * 8-link device 
+		 *
+		 */
+		if( capacity == 4 ){
+	
+			switch( bsize )
+			{
+				case 32:
+					/* [9:7] */
+					tmp = (uint32_t)((addr>>7) & 0x7);
+					break;
+				case 64:
+					/* [10:8] */
+					tmp = (uint32_t)((addr>>8) & 0x7);
+					break;
+				case 128:
+					/* [11:9] */
+					tmp = (uint32_t)((addr>>9) & 0x7);
+					break;
+				default:
+					break;
+			}
+
+			
+		} else if( capacity == 8 ){ 
+
+			switch( bsize )
+			{
+				case 32:
+					/* [9:7] */
+					tmp = (uint32_t)((addr>>7) & 0x7);
+					break;
+				case 64:
+					/* [10:8] */
+					tmp = (uint32_t)((addr>>8) & 0x7);
+					break;
+				case 128:
+					/* [11:9] */
+					tmp = (uint32_t)((addr>>9) & 0x7);
+					break;
+				default:
+					break;
+			}
+
+		}
+	} else {	
+		return -1;
+	}
+
+	/* 
+	 * write out the value 
+	 * 
+ 	 */
+	*quad = tmp;
+
+	return 0;
+}
+
 /* ----------------------------------------------------- HMCSIM_UTIL_DECODE_VAULT */
 /* 
  * HMCSIM_UTIL_DECODE_VAULT
@@ -108,16 +246,16 @@ extern int hmcsim_util_decode_vault( 	struct hmcsim_t *hmc,
 			switch( bsize )
 			{
 				case 32:
-					/* [8:5] */
-					tmp = (uint32_t)((addr>>5) & 0xF);
+					/* [6:5] */
+					tmp = (uint32_t)((addr>>5) & 0x3);
 					break;
 				case 64:
-					/* [9:6] */
-					tmp = (uint32_t)((addr>>6) & 0xF);
+					/* [7:6] */
+					tmp = (uint32_t)((addr>>6) & 0x3);
 					break;
 				case 128:
-					/* [10:7] */
-					tmp = (uint32_t)((addr>>7) & 0xF);
+					/* [8:7] */
+					tmp = (uint32_t)((addr>>7) & 0x3);
 					break;
 				default:
 					break;
@@ -128,16 +266,16 @@ extern int hmcsim_util_decode_vault( 	struct hmcsim_t *hmc,
 			switch( bsize )
 			{
 				case 32:
-					/* [12:9] */
-					tmp = (uint32_t)((addr>>9) & 0xF);
+					/* [10:9] */
+					tmp = (uint32_t)((addr>>9) & 0x3);
 					break;
 				case 64:
-					/* [13:10] */
-					tmp = (uint32_t)((addr>>10) & 0xF);
+					/* [11:10] */
+					tmp = (uint32_t)((addr>>10) & 0x3);
 					break;
 				case 128:
-					/* [14:11] */
-					tmp = (uint32_t)((addr>>11) & 0xF);
+					/* [12:11] */
+					tmp = (uint32_t)((addr>>11) & 0x3);
 					break;
 				default:
 					break;
@@ -154,16 +292,16 @@ extern int hmcsim_util_decode_vault( 	struct hmcsim_t *hmc,
 			switch( bsize )
 			{
 				case 32:
-					/* [9:5] */
-					tmp = (uint32_t)((addr>>5) & 0x7);
+					/* [6:5] */
+					tmp = (uint32_t)((addr>>5) & 0x3);
 					break;
 				case 64:
-					/* [10:6] */
-					tmp = (uint32_t)((addr>>6) & 0x7);
+					/* [7:6] */
+					tmp = (uint32_t)((addr>>6) & 0x3);
 					break;
 				case 128:
-					/* [11:7] */
-					tmp = (uint32_t)((addr>>7) & 0x7);
+					/* [8:7] */
+					tmp = (uint32_t)((addr>>7) & 0x3);
 					break;
 				default:
 					break;
@@ -175,16 +313,16 @@ extern int hmcsim_util_decode_vault( 	struct hmcsim_t *hmc,
 			switch( bsize )
 			{
 				case 32:
-					/* [9:5] */
-					tmp = (uint32_t)((addr>>5) & 0x1F);
+					/* [6:5] */
+					tmp = (uint32_t)((addr>>5) & 0x3);
 					break;
 				case 64:
-					/* [10:6] */
-					tmp = (uint32_t)((addr>>6) & 0x1F);
+					/* [7:6] */
+					tmp = (uint32_t)((addr>>6) & 0x3);
 					break;
 				case 128:
-					/* [10:7] */
-					tmp = (uint32_t)((addr>>7) & 0xF);
+					/* [8:7] */
+					tmp = (uint32_t)((addr>>7) & 0x3);
 					break;
 				default:
 					break;
@@ -200,6 +338,41 @@ extern int hmcsim_util_decode_vault( 	struct hmcsim_t *hmc,
 	 * 
  	 */
 	*vault = tmp;
+
+	return 0;
+}
+
+/* ----------------------------------------------------- HMCSIM_UTIL_DECODE_QV */
+/* 
+ * HMCSIM_UTIL_DECODE_QV
+ * 
+ */
+extern int hmcsim_util_decode_qv( 	struct hmcsim_t *hmc, 
+					uint32_t dev, 
+					uint32_t bsize, 
+					uint64_t addr,
+					uint32_t *quad, 
+					uint32_t *vault )
+{
+
+	if( hmc == NULL ){ 
+		return -1;
+	}
+
+	if( quad == NULL ){ 
+		return -1;
+	}
+
+	if( vault == NULL ){ 
+		return -1;
+	}
+
+	/* 
+	 * decode the quad and vault
+	 * 
+ 	 */
+	hmcsim_util_decode_quad( hmc, dev, bsize, addr, quad );
+	hmcsim_util_decode_vault( hmc, dev, bsize, addr, vault );
 
 	return 0;
 }
