05dee8185ab325eef6d58d55e3c90a35f0b43d90
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-01-05 02:46:19 +0000

    moving gsim_free_sim function to the library object

---
+++ trunk/sim/src/gsim_free_sim.c
@@ -0,0 +1,71 @@
+/* 
+ * 
+ * _GSIM_FREE_ 
+ * 
+ * GOBLIN-SIM LIBRARY FREE FUNCTIONS
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "goblin_sim.h"
+
+
+/* ----------------------------------------------------------- GSIM_FREE_SIM */
+/* 
+ * GSIM_FREE_SIM
+ * 
+ */
+extern void gsim_free_sim( struct gsim_t *sim )
+{
+#ifdef GSIM_TRACE
+	GSIM_PRINT_FUNC_ENTRY();
+#endif
+
+	if( sim == NULL ){
+		/* nothing to do, return */
+		return ;
+	}
+
+	if( sim->hw != NULL ){ 
+		gsim_free( sim->hw->__ptr_partition );
+		gsim_free( sim->hw->__ptr_node );
+		gsim_free( sim->hw->__ptr_socket );
+		gsim_free( sim->hw->__ptr_task_group );
+		gsim_free( sim->hw->__ptr_task_proc );
+		gsim_free( sim->hw->__ptr_task );
+		gsim_free( sim->hw->__ptr_icache );
+		gsim_free( sim->hw );
+	}
+
+	/* 
+	 * close all the file handles
+	 * 
+	 */
+
+	if( sim->tfile != NULL ){ 
+		fflush( sim->tfile );	
+		fclose( sim->tfile );
+		sim->tfile = NULL;
+	}
+
+	if( sim->lfile != NULL ){ 
+		fflush( sim->lfile );
+		fclose( sim->lfile );
+		sim->lfile = NULL;
+	}
+
+	if( sim != NULL ){ 
+		free( sim );
+		sim = NULL;
+	}
+
+#ifdef GSIM_TRACE
+	GSIM_PRINT_FUNC_EXIT();
+#endif
+	return ;
+}
+
+/* EOF */
