bc832b87bd623c1967e67d8ea524dd49267fc281
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-01-07 04:05:29 +0000

    fixed issue in retrieving the nrows from an assembly file

--- trunk/sim/src/gasm_row.c
+++ trunk/sim/src/gasm_row.c
@@ -56,29 +56,31 @@ extern uint64_t gasm_row_walker( char *afile )
 		fgets( buf, 2048, ifile );
 
 		if( (strlen( buf ) > 2) && (buf[0] != '#') ){
-			
+
+			printf( "buffer = %s\n", buf );		
+	
 			/* 
 			 * parse the line and look for immediates
 			 * immediates appear as '$value'
 			 */
-			tmp = strtok( buf, "$" );
+			nrows++;
 
-			if( tmp != NULL ){
-				/* 
-				 * immediate present 
-				 */
-				nrows += 2;
-			}else{ 
-				/* 
-				 * immediate not present 
-				 */
-				nrows += 1;
+			tmp = strpbrk( buf, "$" );
+			while( tmp != NULL ){ 
+				nrows++;
+				tmp = strpbrk( tmp+1, "$" );
 			}
 		}
 
 		memset( buf, 0, sizeof( char ) * 2048 );
 		tmp = NULL;
 	}
+
+	/* 
+	 * rewind the file
+	 * 
+	 */
+	rewind( ifile );
 
 	/* 
 	 * close the file
