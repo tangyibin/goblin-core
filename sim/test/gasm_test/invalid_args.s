68a3d14718ead55ee2c4f77de84e56f7e3a4d2f3
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-01-15 03:23:04 +0000

    adding new tests for the gasm assembler

---
+++ sim/test/gasm_test/invalid_args.s
@@ -0,0 +1,5 @@
+#-- invalid_args.s
+#-- 
+#-- test that induces an invalid argument error 
+
+nop %r0
---
+++ sim/test/gasm_test/invalid_vector.s
@@ -0,0 +1,5 @@
+#-- invalid_vector.s
+#-- 
+#-- induces an invalid vector register argument from gasm
+
+rtna %v0
