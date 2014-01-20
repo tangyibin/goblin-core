69984f9efcc128cfc479f053603e9447d9004016
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-01-20 02:45:49 +0000

    first commit of mem_sim source tree for testing the various memory interface theories

---
+++ trunk/sim/mem_sim/include/mem_sim.h
@@ -0,0 +1,39 @@
+/* 
+ * _MEM_SIM_H_
+ * 
+ * GC64 MEMORY INTERFACE SIMULATION MODULE
+ * 
+ * MAIN HEADER FILE 
+ * 
+ */
+
+/*!	\file mem_sim.h
+	\brief GoblinCore-64 Memory Interface Simulation Library Header FIle
+
+	The MEM_SIM library is a standard C library that provides functional
+	simulation support for experimenting with various memory interface
+	modules in association with the GoblinCore-64 architecture.  
+
+*/
+
+#ifndef _MEM_SIM_H_
+#define _MEM_SIM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <sys/types.h>
+#include "mem_sim_types.h"
+#include "mem_sim_macros.h"
+
+/* ------------------------------------------------ FUNCTION PROTOTYPES */
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* _MEM_SIM_H_ */
+
+/* EOF */
---
+++ trunk/sim/mem_sim/include/mem_sim_macros.h
@@ -0,0 +1,39 @@
+/* 
+ * _MEM_SIM_MACROS_H_ 
+ * 
+ * MEMORY INTERFACE SIMULATION LIBRARY 
+ * 
+ * MACROS HEADER FILE 
+ * 
+ */
+
+#ifndef _MEM_SIM_MACROS_H_ 
+#define _MEM_SIM_MACROS_H_ 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* ------------------------------------------------ VERSION MACROS */
+#ifndef MEMSIM_MAJOR_VERSION
+#define MEMSIM_MAJOR_VERSION	0
+#endif
+
+#ifndef MEMSIM_MINOR_VERSION
+#define MEMSIM_MINOR_VERSION	1
+#endif
+
+
+/* ------------------------------------------------ RETURN CODES */
+#define		MEMSIM_ERROR	-1
+#define		MEMSIM_OK	0
+
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* _MEM_SIM_MACROS_H_ */
+
+/* EOF */
