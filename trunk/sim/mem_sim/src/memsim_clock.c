3d2ae10d2ba355886da05f4bbf6dbeb142518293
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-01-26 22:47:22 +0000

    adding placeholders for individual clock backends; beginning work on simple backend

--- trunk/sim/mem_sim/src/memsim_clock.c
+++ trunk/sim/mem_sim/src/memsim_clock.c
@@ -12,6 +12,11 @@
 #include "mem_sim.h"
 
 
+/* ------------------------------------------------ FUNCTION_PROTOTYPES */
+extern int memsim_clock_simple( struct memsim_t *msim );
+extern int memsim_clock_cache( struct memsim_t *msim );
+extern int memsim_clock_exp( struct memsim_t *msim );
+
 /* ------------------------------------------------ MEMSIM_CLOCK */
 /* 
  * MEMSIM_CLOCK
@@ -19,8 +24,7 @@
  */
 extern int memsim_clock( struct memsim_t *msim )
 {
-	/* vars */
-	/* ---- */
+	int rtn	= 0;
 
 	/* 
 	 * sanity check 
@@ -30,8 +34,54 @@ extern int memsim_clock( struct memsim_t *msim )
 		return MEMSIM_ERROR;
 	}
 
+	/* 
+	 * Here we decide which algorithm to 
+ 	 * execute. For each algorithm type, 
+	 * we must provide a backend 
+	 * implementation.  
+	 *
+	 */
+	switch( msim->alg )
+	{
+		case MEMSIM_SIMPLE:
 
-	return MEMSIM_OK;
+			rtn	= memsim_clock_simple(msim);
+
+			/* 
+			 * nudge the clock 
+		 	 *
+			 */
+			msim->clock ++;
+
+			break;
+		case MEMSIM_CACHE:
+
+			rtn	= memsim_clock_cache(msim);
+
+			/* 
+			 * nudge the clock 
+		 	 *
+			 */
+			msim->clock ++;
+
+			break;
+		case MEMSIM_EXP:
+
+			rtn	= memsim_clock_exp(msim);
+
+			/* 
+			 * nudge the clock 
+		 	 *
+			 */
+			msim->clock ++;
+
+			break;
+		default:
+			rtn = MEMSIM_ERROR;
+			break;
+	}
+
+	return rtn;
 }
 
 /* EOF */
