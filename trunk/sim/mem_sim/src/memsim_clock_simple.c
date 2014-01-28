fda9d24ee6d6f4a4131bd277a265cd546bfa53d2
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-01-28 02:12:42 +0000

    adding clock simple global function for the initial simple model

--- trunk/sim/mem_sim/src/memsim_clock_simple.c
+++ trunk/sim/mem_sim/src/memsim_clock_simple.c
@@ -121,6 +121,96 @@ static int memsim_clock_simple_process_amo( struct memsim_t *msim ){
 
 /* ------------------------------------------------ MEMSIM_CLOCK_SIMPLE_PROCESS_GLOBAL */
 static int memsim_clock_simple_process_global( struct memsim_t *msim ){
+
+	/* vars */
+	uint64_t count	= 0x00ll;
+	uint64_t ticks	= 0x00ll;
+	int done	= 0;
+	uint32_t cur	= 0;
+	/* ---- */
+
+	/* 
+	 * process the slot entries within this clock cycle
+	 * 
+	 */
+	while( (done != 1) && (count<msim->hw.payps) ){ 
+	
+		/*
+		 * check the 'cur' element
+	 	 * 
+		 */
+		ticks = 0x00ll;
+
+		if( msim->global->entry[cur].valid == 1 ){ 
+
+			/* 
+			 * cur element is valid, process it 
+			 *
+			 */
+				
+			/* 
+			 * First, determine the number of ticks
+			 * required to process this entry
+			 * If we don't have enough ticks, 
+			 * we're done.
+			 * 
+			 * Each 64-bit element requires a tick
+			 *
+			 */ 
+			if( memsim_tick_count( &(msim->global->entry[cur]), &ticks ) != 0 ){
+				return MEMSIM_ERROR;
+			}
+			
+			if( (count+ticks) <= msim->hw.payps ){ 
+				
+				/* 
+				 * we're within range.
+				 * push the request 
+				 *
+				 */
+
+				/* print the trace */
+				if( (msim->tracelevel & MEMSIM_TRACE_MEMOP ) > 0 ){ 
+					memsim_trace_memop( msim, 
+							&(msim->global->entry[cur]) );
+				}
+
+				/* clear the entry */
+				memsim_clear_entry( &(msim->global->entry[cur]) );
+
+				/* bump the count */
+				count += ticks;
+	
+			}else{
+				/* 
+				 * out of range, time to stall 
+				 *
+				 */
+				done = 1;
+			}
+
+		}
+
+		if( (cur+1) >= msim->global->num_slots ){ 
+			/* 
+			 * Last element
+			 *
+			 */
+			done = 1;
+		}else{ 
+			cur += 1;
+		}
+	
+	} 
+
+	/* 
+	 * bubble the slot entries
+	 * 
+	 */
+	if( memsim_bubble_slot( msim->global ) != 0 ){ 
+		return MEMSIM_ERROR;
+	}
+
 	return MEMSIM_OK;
 }
 
@@ -135,7 +225,7 @@ static int memsim_clock_simple_process_taskgroup( struct memsim_t *msim, uint32_
  * 
  * 
  * This backend implementation does nothing but pass
- * the slot entries between queues. It does not request coalescing 
+ * the slot entries between queues. It does no request coalescing 
  * whatsoever.  
  * 
  */
