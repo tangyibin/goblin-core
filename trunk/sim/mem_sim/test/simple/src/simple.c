884fcdc6134f20a22939305e253d81cdc269ab9c
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-02-03 03:14:22 +0000

    adding placeholder for simple test

---
+++ trunk/sim/mem_sim/test/simple/src/simple.c
@@ -0,0 +1,17 @@
+/* 
+ * _SIMPLE_C_ 
+ * 
+ * MEMSIM SIMPLE TEST
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mem_sim.h"
+
+/* ------------------------------------------- MAIN */
+int main( int argc, char **argv )
+{
+	return 0;
+}
+/* EOF */
