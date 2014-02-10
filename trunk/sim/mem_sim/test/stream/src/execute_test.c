513f600cdb23008678ea452705c7ab4364135d1e
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-02-10 00:07:20 +0000

    adding test harness placeholders for stream triad; fixing small bug in options parsing for simple test; updating makefiles for tests

---
+++ trunk/sim/mem_sim/test/stream/src/execute_test.c
@@ -0,0 +1,15 @@
+/* 
+ * _EXECUTE_TEST_C_ 
+ * 
+ * MEMSIM STREAM TEST EXECUTION 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+#include "mem_sim.h"
+
+
+/* EOF */
