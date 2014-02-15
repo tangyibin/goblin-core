df786be6a1367cfedb08cef672d1017b1825b3e8
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-02-15 16:38:28 +0000

    basic function structure in order to get the gups test building

---
+++ sim/mem_sim/test/gups/src/execute_test.c
@@ -0,0 +1,29 @@
+/* _EXECUTE_TEST_C_ 
+ * 
+ * MEMSIM GUPS TEST EXECUTION 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+#include "mem_sim.h"
+
+/* --------------------------------------------- STATUS MACROS */
+/* --------------------------------------------- STRUCTURES */
+/* --------------------------------------------- EXECUTE_TEST */
+/* 
+ * EXECUTE TEST
+ * 
+ * Executes an unbinned GUPS : 
+ *
+ * 
+ */
+extern int execute_test(	struct memsim_t *msim, 
+				uint64_t *gconst, 
+				uint32_t num_threads, 
+				long num_req ){ 
+	return 0;
+}
+/* EOF */
