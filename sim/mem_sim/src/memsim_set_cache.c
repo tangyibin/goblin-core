1f353db3071ad4d32a1f970de3fe612da8d62ef2
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-03-02 13:14:56 +0000

    adding cache initialization function

--- sim/mem_sim/src/memsim_set_cache.c
+++ sim/mem_sim/src/memsim_set_cache.c
@@ -12,6 +12,49 @@
 #include "mem_sim.h"
 
 
+/* ------------------------------------------------ MEMSIM_INIT_CACHE */
+/* 
+ * MEMSIM_INIT_CACHE
+ * 
+ */
+static int memsim_init_cache( struct memsim_t *msim, 
+				struct memsim_cache_t *level, 
+				uint8_t ways, 
+				uint32_t sets ) {
+
+	/* vars */
+	uint32_t i	= 0;
+	/* ---- */
+
+	if( msim == NULL ){ 
+		return MEMSIM_ERROR;
+	}
+
+	if( level == NULL ){ 
+		return MEMSIM_ERROR;
+	}
+
+	level->ways	= ways;
+	level->sets	= sets;
+
+	/* init each line */
+	for( i=0; i<((uint32_t)(ways)*sets); i++ ){ 
+		level->cache->valid	= 0x0;
+		level->cache->tag	= 0x00;
+		level->cache->clock	= 0x00ll;
+		level->cache->data[0]	= 0x00ll;	
+		level->cache->data[1]	= 0x00ll;	
+		level->cache->data[2]	= 0x00ll;	
+		level->cache->data[3]	= 0x00ll;	
+		level->cache->data[4]	= 0x00ll;	
+		level->cache->data[5]	= 0x00ll;	
+		level->cache->data[6]	= 0x00ll;	
+		level->cache->data[7]	= 0x00ll;	
+	}	
+
+	return 0;
+}
+
 /* ------------------------------------------------ MEMSIM_SET_CACHE */
 /* 
  * MEMSIM_SET_CACHE
@@ -25,6 +68,7 @@ extern int memsim_set_cache(	struct memsim_t *msim,
 {
 	/* vars */
 	struct memsim_cache_t *ptr	= NULL;
+	struct memsim_line_t *lines	= NULL;
 	/* ---- */
 
 	/* 
@@ -39,11 +83,20 @@ extern int memsim_set_cache(	struct memsim_t *msim,
 		return -1;
 	}
 
-	ptr	= malloc( sizeof( struct memsim_cache_t ) *
-				(ways * sets) );
+	ptr	= malloc( sizeof( struct memsim_cache_t ) );
 	if( ptr == NULL ){ 
 		return -1;
 	}
+
+
+	lines 	= malloc( sizeof( struct memsim_line_t ) * (ways*sets));
+	if( lines == NULL ){ 
+		return -1;
+	}
+
+	ptr->cache = lines;
+
+	memsim_init_cache( msim, ptr, ways, sets );
 
 	if( unit == 1 ){ 
 		msim->l1_cache_sets 	= sets;
