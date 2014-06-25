d9e07be113dd7a30613d66606d31bebf85fbf7ff
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-06-25 01:23:08 +0000

    adding csr formatting; updating command line parsing; adding placeholders for readmat functions and execute_test functions; updating makefile; builds clean

---
+++ sim/hmc_sim/test/spmv/src/readmat.c
@@ -0,0 +1,18 @@
+/* 
+ * _READMAT_C_ 
+ * 
+ * HMC-SIM SPMV MATRIX UTILITY FUNCTIONS 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "spmv.h"
+
+/* ------------------------------------------------- READMAT */
+extern int readmat( char *matfile, struct csr_t *mat )
+{
+	return 0;
+}
+
+/* EOF */
