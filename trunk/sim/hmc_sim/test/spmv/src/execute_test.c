6ec548d07887ee2138336afae9fd3d6dd903a983
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-06-25 01:23:08 +0000

    adding csr formatting; updating command line parsing; adding placeholders for readmat functions and execute_test functions; updating makefile; builds clean

---
+++ trunk/sim/hmc_sim/test/spmv/src/execute_test.c
@@ -0,0 +1,25 @@
+/* 
+ * _EXECUTE_TEST_C_ 
+ * 
+ * HMC-SIM SPMV TEST EXECUTION FUNCTIONS
+ * FOR THE FINITE STATE MACHINE 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "spmv.h"
+#include "hmc_sim.h"
+
+
+/* ------------------------------------------ EXECUTE_TEST */
+extern int execute_test( struct hmcsim_t *hmc, 
+			struct csr_t *mat, 
+			uint32_t num_threads, 
+			uint32_t simd )
+{
+	return 0;
+}
+
+/* EOF */
