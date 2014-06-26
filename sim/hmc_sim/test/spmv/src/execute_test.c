ca276d3571f2b2560eb4b531f986011f5faa9574
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-06-26 02:33:47 +0000

    adding small utility functions to spmv test

--- sim/hmc_sim/test/spmv/src/execute_test.c
+++ sim/hmc_sim/test/spmv/src/execute_test.c
@@ -13,12 +13,40 @@
 #include "hmc_sim.h"
 
 
+/* ------------------------------------------ ZERO_PACKET */
+/* 
+ * ZERO_PACKET 
+ * 
+ */
+static void zero_packet( uint64_t *packet )
+{
+	uint64_t i	= 0x00ll;
+
+	/* 
+	 * zero the packet 
+	 * 
+ 	 */
+	for( i=0; i<HMC_MAX_UQ_PACKET; i++ ){ 
+		packet[i] = 0x00ll;
+	}
+
+	return ;
+}
+
 /* ------------------------------------------ EXECUTE_TEST */
+/* 
+ * executes a sparse matrix vector multiply 
+ * using a compressed sparse row format 
+ * 
+ */
 extern int execute_test( struct hmcsim_t *hmc, 
 			struct csr_t *mat, 
 			uint32_t num_threads, 
 			uint32_t simd )
 {
+	/* vars */
+	/* ---- */
+
 	return 0;
 }
 
