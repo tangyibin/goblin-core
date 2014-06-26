3f0e38780bf27e67cac62d2bede8d15ffcec93a8
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-06-26 02:33:47 +0000

    adding small utility functions to spmv test

--- trunk/sim/hmc_sim/test/spmv/src/readmat.c
+++ trunk/sim/hmc_sim/test/spmv/src/readmat.c
@@ -9,9 +9,34 @@
 #include <stdlib.h>
 #include "spmv.h"
 
+/* ------------------------------------------------- ALLOCATE MEMORY */
+
+
 /* ------------------------------------------------- READMAT */
+/* 
+ * reads an interprets the spmv input file 
+ * 
+ */
 extern int readmat( char *matfile, struct csr_t *mat )
 {
+	/* vars */
+	FILE *infile	= NULL;
+	/* ---- */
+
+	if( matfile == NULL ){ 
+		return -1;
+	}else if( mat == NULL ){ 
+		return -1;
+	}
+
+	infile = fopen( matfile, "r" );
+	if( infile == NULL ){ 
+		printf( "ERROR : could not open matfile at %s\n", matfile );
+		return -1;
+	}
+
+	fclose( infile );
+
 	return 0;
 }
 
