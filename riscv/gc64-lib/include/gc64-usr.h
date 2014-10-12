2225dab8d43d0c1ea9a0ec8aab3a9fa21032066f
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-10-12 20:25:22 +0000

    first commit of something for syslib

---
+++ riscv/gc64-lib/include/gc64-usr.h
@@ -0,0 +1,38 @@
+/* 
+ * _GC64-USR_H_ 
+ * 
+ * GC64 USER INTERFACE HEADER
+ * 
+ * Provides basic user interface functionality 
+ * 
+ * 
+ */
+
+#ifndef _GC64-USR_H_
+#define _GC64-USR_H_
+
+/* 
+ * REQUIRED HEADERS
+ * 
+ */
+#include "gc64-types.h"
+
+/* 
+ * NAMED ADDRESS SPACE MACROS 
+ * 
+ */
+#ifdef __SCRATCH
+#define __scratch typedef __attribute__((address_space(200))) 
+#define __SCRATCH typedef __attribute__((address_space(200))) 
+#define
+
+/* 
+ * 
+ * FUNCTIONS 
+ * 
+ */
+
+
+
+#endif /* _GC64-USR_H_ */
+/* EOF */
