197f159e4d37f0e50f8618bbbdb131b32c6bd890
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-10-15 01:10:47 +0000

    adding the base data structures to contain the scratchpad memories; adding the init routine for the scratchpad

--- riscv/gc64-lib/lib/gc64complib/gc64_constructor.c
+++ riscv/gc64-lib/lib/gc64complib/gc64_constructor.c
@@ -16,6 +16,14 @@
  */
 __attribute__ ((constructor(0))) __attribute((__section__(".init.text"))) static void gc64_run_constructor( void );
 
+/* 
+ * STATIC VOID GC64_RUN_CONSTRUCTOR
+ * 
+ * STAGE 1: INITIATE THE DATA STRUCTURE
+ * STAGE 2: INITIATE THE SCRATCHPAD MEMORY STRUCTURE
+ * STAGE 3: WALK THE OBEJCT FILE AND INSERT THE OBJECTS INTO SCRATCHPAD 
+ *
+ */
 static void 	gc64_run_constructor( void ) {
 	/* VARS */
 	struct gc64comp_t *comp	= NULL;
@@ -37,6 +45,23 @@ static void 	gc64_run_constructor( void ) {
 			exit( -1 );
 		}
 	}
+
+	/* 
+	 * STAGE 1: INIT THE DATA STRUCTURE 
+	 * 
+	 */
+	comp->mem	= NULL;
+	comp->status	= 0x00ll;
+
+	/*
+	 * STAGE 2: INIT THE SCRATCHPAD MEMORY STRUCTURE
+	 * 
+	 */
+
+	/* 
+	 * STAGE 3: WALK THE DATA STRUCTURE AND INSERT THE SCRATCHPAD OBJECTS
+	 * 
+	 */
 
 	/* 
 	 * set the global to the new structure
---
+++ riscv/gc64-lib/lib/gc64complib/gc64_spad.c
@@ -0,0 +1,57 @@
+/* 
+ * _GC64_SPAD_C_ 
+ * 
+ * GC64 COMPILER LIBRARY SCRATCHPAD FUNCTIONS 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "gc64-comp.h"
+
+
+#define	GC64_BASE_ADDR		0x0000000080000000
+#define	GC64_DEFAULT_SIZE	20000000
+
+/* 
+ * EXTERN INT GC64_SPAD_INIT( STRUCT GC64COMP_T *COMP )
+ * 
+ *
+ */
+extern int gc64_spad_init( struct gc64comp_t *comp ){ 
+
+	/* sanity check */
+	if( comp == NULL ){ 
+		return -1;
+	}
+
+	if( comp->mem != NULL ){ 
+		return -1;
+	}	
+
+	/* 
+	 * allocate memory for the memory structure 
+	 * 
+	 */
+	comp->mem	= malloc( sizeof( struct gc64sp_t ) );
+	if( comp->mem == NULL ){
+		return -1;
+	}
+
+	/* 
+	 * init the values 
+	 * 
+	 */
+	comp->mem->base_addr	= GC64_BASE_ADDR;
+	comp->mem->tsize	= GC64_DEFAULT_SIZE;
+	comp->mem->used		= 0x00ll;
+	comp->mem->free		= GC64_DEFAULT_SIZE;
+	comp->mem->nentry	= 0x00ll;
+	comp->mem->entry	= NULL;
+	
+
+	return 0;
+}
+
+
+/* EOF */
