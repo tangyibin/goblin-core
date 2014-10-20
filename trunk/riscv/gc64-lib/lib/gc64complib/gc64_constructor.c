2e29c8f11c9ee3bfb689e8c13093c3ad117942af
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-10-20 21:12:31 +0000

    adding separate init function for smoke testing

--- trunk/riscv/gc64-lib/lib/gc64complib/gc64_constructor.c
+++ trunk/riscv/gc64-lib/lib/gc64complib/gc64_constructor.c
@@ -17,62 +17,8 @@
 __attribute__ ((constructor(0))) __attribute((__section__(".init.text"))) static void gc64_run_constructor( void );
 
 
-/* 
- * EXTERN INT __GC64_INIT( VOID ) 
- * 
- * STAGE 1: INITIATE THE DATA STRUCTURE
- * STAGE 2: INITIATE THE SCRATCHPAD MEMORY STRUCTURE
- * STAGE 3: WALK THE OBEJCT FILE AND INSERT THE OBJECTS INTO SCRATCHPAD 
- *
- */
-extern int __gc64_init( void ){ 
-
-	/* VARS */
-	struct gc64comp_t *comp	= NULL;
-	/* ---- */
-
-	if( __g_comp != NULL ){ 
-		/* 
-		 * an error has occurred, abort
-	 	 *
-		 */
-		exit( -1 );
-	}else{ 
-		comp = malloc( sizeof( struct gc64comp_t ) );
-		if( comp == NULL ){ 
-			/* 
-			 * error occurred, abort
-			 *
-			 */
-			exit( -1 );
-		}
-	}
-
-	/* 
-	 * STAGE 1: INIT THE DATA STRUCTURE 
-	 * 
-	 */
-	comp->mem	= NULL;
-	comp->status	= 0x00ll;
-
-	/*
-	 * STAGE 2: INIT THE SCRATCHPAD MEMORY STRUCTURE
-	 * 
-	 */
-
-	/* 
-	 * STAGE 3: WALK THE DATA STRUCTURE AND INSERT THE SCRATCHPAD OBJECTS
-	 * 
-	 */
-
-	/* 
-	 * set the global to the new structure
-	 * 
-	 */
-	__g_comp	= comp;
-
-	return GC64_OK;
-}
+/* FUNCTION PROTOTYPES */
+extern int __gc64_init();
 
 /* 
  * STATIC VOID GC64_RUN_CONSTRUCTOR
