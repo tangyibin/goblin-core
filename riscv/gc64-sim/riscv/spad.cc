b60f76eabc79858132465a0ae7e4eeb1e0695f7d
john.leidel@gmail.com <john.leidel@gmail.com@e6ba0856-4ba8-cdcd-91e0-d07434e8c6d2>
2014-10-21 20:28:56 +0000

    fixing support for spad address decoding; spad is fully fused to core simulator; adding build support in configure script

--- riscv/gc64-sim/riscv/spad.cc
+++ riscv/gc64-sim/riscv/spad.cc
@@ -13,6 +13,17 @@
  * default constructor 
  * 
  */
+spad_t::spad_t() {
+	spadsz		= 0;
+	base_addr	= 0;
+	align		= 0;	
+	stor		= NULL;
+}
+
+/* 
+ * constructor 
+ * 
+ */
 spad_t::spad_t( size_t _spadsz, uint64_t _base_addr, size_t _align ) {
 
 	spadsz		= _spadsz;
@@ -42,9 +53,50 @@ spad_t::~spad_t() {
  * spad_translate
  * 
  */
-void * spad_t::translate( uint64_t _addr, bool load, bool store ) { 
+void * spad_t::translate( uint64_t _addr, bool store, bool fetch ) { 
+
+	/* vars */
+	uint64_t upper_addr	= base_addr + spadsz;
+	uint64_t stride		= 0x00ll;
+	int elem		= 0x00;
+	char *tstor		= NULL;
+	/* ---- */
 	
+	/* 
+	 * check the bounds of the address request
+	 * 
+	 */	
+	if( 	(_addr < base_addr) || 
+		(_addr > upper_addr) ){ 
+		
+		return NULL;
+	} 
+
+	/* 
+	 * get the stride into the physical storage 
+	 * 
+	 */
+	stride 	= _addr - base_addr;
+	elem	= stride/(sizeof(char));
+
+	/* 
+	 * todo: check the data alignment 
+	 * 
+ 	 */
+
+	/* 
+	 * retrieve a full 64-bit payload value
+	 * the caller must understand which portion 
+	 * belongs to them 
+	 *
+ 	 */
+	tstor		= (char *)(&(stor[elem]));
+
+	return (void *)(tstor);
 }
+
+/* DEPRECATED */
+#if 0
 
 /* 
  * load operation 
@@ -118,5 +170,6 @@ void spad_t::store( spad_value value, uint64_t _addr, int *err ){
 		return ;
 	} 
 }
+#endif
 
 /* EOF */
